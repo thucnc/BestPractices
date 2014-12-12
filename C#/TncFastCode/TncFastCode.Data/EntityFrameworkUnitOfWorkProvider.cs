7d24d17d4e91bd5a8dd6cd8f93d9103635c84db4
thucnc <ngcanhthuc@yahoo.com>
2014-12-12 21:57:47 +0700

    some best practices for infrastructure embeded in form of libraries to quick start new app

---
+++ C#/TncFastCode/TncFastCode.Data/EntityFrameworkUnitOfWorkProvider.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+
+
+namespace TncFastCode.Data
+{
+    public class EntityFrameworkUnitOfWorkProvider<T> : IUnitOfWork where T : DbContext
+    {
+        private readonly T _db;
+        private readonly string _connectionString;
+        private readonly int _commandTimeout;
+
+        public EntityFrameworkUnitOfWorkProvider(string connectionString)
+            : this(connectionString, -1)
+        {
+            // Nothing more to do
+        }
+
+        public EntityFrameworkUnitOfWorkProvider(string connectionString, int commandTimeout)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException("connection string must have a value");
+            }
+            _connectionString = connectionString;
+            _commandTimeout = commandTimeout;
+
+            var t = typeof(T);
+            _db = (T)Activator.CreateInstance(t, new object[] { connectionString });
+
+            ((IObjectContextAdapter)_db).ObjectContext.CommandTimeout =
+                commandTimeout < 0 ? _db.Database.Connection.ConnectionTimeout : commandTimeout;
+        }
+
+        public IQueryable<TEntity> Table<TEntity>() where TEntity : class
+        {
+            return _db.Set<TEntity>();
+        }
+
+        public void DeleteAllOnSubmit<TEntity>(IEnumerable<TEntity> entities) where TEntity : class
+        {
+            entities.ToList().ForEach(entity => _db.Set<TEntity>().Remove(entity));
+        }
+
+        public void DeleteOnSubmit<TEntity>(TEntity entity) where TEntity : class
+        {
+            _db.Set<TEntity>().Remove(entity);
+        }
+
+        public void InsertOnSubmit<TEntity>(TEntity entity) where TEntity : class
+        {
+            _db.Set<TEntity>().Add(entity);
+        }
+
+        public void SubmitChanges()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public IUnitOfWork CreateNewInstance()
+        {
+            return new EntityFrameworkUnitOfWorkProvider<T>(_connectionString, _commandTimeout);
+        }
+    }
+}
---
+++ C#/TncFastCode/TncFastCode.Data/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TncFastCode.Data
+{
+    public interface IUnitOfWork
+    {
+        IQueryable<T> Table<T>() where T : class;
+
+        void DeleteAllOnSubmit<T>(IEnumerable<T> entities) where T : class;
+
+        void DeleteOnSubmit<T>(T entity) where T : class;
+
+        void InsertOnSubmit<T>(T entity) where T : class;
+
+        void SubmitChanges();
+
+        IUnitOfWork CreateNewInstance();
+    }
+}
